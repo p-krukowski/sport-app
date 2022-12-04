c0380c136c5cff3cdbc39ce9d3ff6b18e258f368
Przemysław Krukowski <przem.kruk@gmail.com>
2022-12-04 15:25:52 +0100

    fix date in entry comment dto

--- sport-app-server/src/main/java/com/sportapp/demo/models/dtos/social/EntryCommentGetDto.java
+++ sport-app-server/src/main/java/com/sportapp/demo/models/dtos/social/EntryCommentGetDto.java
@@ -1,6 +1,5 @@
 package com.sportapp.demo.models.dtos.social;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,8 +13,6 @@ public class EntryCommentGetDto {
   private AuthorDto author;
   private int score;
   private String imageUrl;
-
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy HH:mm")
   private LocalDateTime createdAt;
 
 }
